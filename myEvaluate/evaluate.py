fc11109bab0a7940a805cdfd5f33b28b4c4edb80
wyett-ping <wyettzeng@gmail.com>
2024-11-12 06:31:46 +0000

    new scripts

--- myEvaluate/evaluate.py
+++ myEvaluate/evaluate.py
@@ -1,51 +1,53 @@
-from codegen.generate import main as create_inference
+from evalplus.codegen import run_codegen as create_inference
 from evalplus.sanitize import script as sanitize
-from evalplus.evaluate import main as evaluate_model
+from evalplus.evaluate import evaluate as evaluate_model
 from fire import Fire
 # tuple in the form of (original model path, original model name, custom model path, custom model name)
 MODELS = {
-    "llama_3_instruct_8b_dpo_full": (
-        "meta-llama/Meta-Llama-3-8B-Instruct",
-        "trained_models/llama_3_instruct/full/dpo/1",
-    ),
-    "llama_3_instruct_8b_apo_full": (
-        "meta-llama/Meta-Llama-3-8B-Instruct",
-        "trained_models/llama_3_instruct/full/apo_zero/1",
-    ),
-    "mistral_3_instruct_dpo_full": (
-        "mistralai/Mistral-7B-Instruct-v0.3",
-        "trained_models/mistral_3_instruct/full/dpo/3",
-    ),
+    "codellama_instruct_7b": "codellama/CodeLlama-7b-Instruct-hf",
+    "mistral_instruct_v3_7b": "mistralai/Mistral-7B-Instruct-v0.3",
+    "llama3_instruct_8b": "meta-llama/Meta-Llama-3-8B-Instruct",
+    "nxcode_cq_orpo_7b": "NTQAI/Nxcode-CQ-7B-orpo",
+    "code_qwen_v1.5_7b": "Qwen/CodeQwen1.5-7B-Chat",
+    "qwen_coder_2.5_7b": "Qwen/Qwen2.5-Coder-7B-Instruct"
 }
 
 
-def evaluate(model_name: str, custom: bool, evaluate:bool):
-    original_model_path, custom_model_path = MODELS[model_name]
-    if custom:
-        model_path = custom_model_path
-    else:
-        model_path = original_model_path
+def evaluate(model_name: str, n_samples:int = 1):
+    model_path = MODELS[model_name]
     model_path_2 = model_path.replace("/", "--")
     
     for dataset in ["humaneval", "mbpp"]:
-        if not evaluate:
+        if n_samples == 1:
             create_inference(
                 model=model_path,
                 greedy=True,
                 root=f"inferenced_output",
                 jsonl_fmt=True,
                 dataset=dataset,
-                backend="vllm"
+                backend="vllm",
             )
             sanitize(samples=f"inferenced_output/{dataset}/{model_path_2}_vllm_temp_0.0.jsonl")
         else:
-            print(f"-----------{dataset}--------------")
-            print("---------------Pre-finetuned-----------------")
-            ram = model_path.replace("/", "--")
+            create_inference(
+                model=model_path,
+                greedy=False,
+                root=f"inferenced_output",
+                jsonl_fmt=True,
+                dataset=dataset,
+                backend="vllm",
+                n_samples=n_samples,
+                temperature=1.0,
+            )
+            sanitize(samples=f"inferenced_output/{dataset}/{model_path_2}_vllm_temp_1.0.jsonl")
+        
+    for dataset in ["humaneval", "mbpp"]:
+        print(f"-----------{dataset}-{model_name}-------------")
+        ram = model_path.replace("/", "--")
+        if n_samples == 0:
             evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{ram}_vllm_temp_0.0-sanitized.jsonl")
-            print("---------------Post-finetuned----------------")
-            ram = custom_model_path.replace("/", "--")
-            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{ram}_vllm_temp_0.0-sanitized.jsonl")
+        else:
+            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{ram}_vllm_temp_1.0-sanitized.jsonl")
 
         
 if __name__ == "__main__":
--- myEvaluate/evaluate.sh
+++ myEvaluate/evaluate.sh
@@ -1,5 +1,33 @@
-model_name=mistral_3_instruct_dpo_full
+#!/bin/bash
 
-python myEvaluate/evaluate.py --model_name=$model_name --custom=True --evaluate=False
-python myEvaluate/evaluate.py --model_name=$model_name --custom=False --evaluate=False
-python myEvaluate/evaluate.py --model_name=$model_name --custom=False --evaluate=True
+MAX_RETRY=3
+models=(
+    "codellama_instruct_7b"
+    "mistral_instruct_v3_7b"
+    "llama3_instruct_8b"
+    "qwen_coder_2.5_7b"
+    "nxcode_cq_orpo_7b"
+    "code_qwen_v1.5_7b"
+    # "deepseek_coder_v2_16b"
+)
+model_name=${models[$1]}
+
+ct=0
+until python myEvaluate/evaluate.py --model_name=$model_name --n_samples=1
+do
+    ct=$((ct + 1))
+    echo "failed ${ct} times"
+    if [ $ct -ge $MAX_RETRY ]; then
+        break
+    fi
+done
+
+ct=0
+until python myEvaluate/evaluate.py --model_name=$model_name --n_samples=16
+do
+    ct=$((ct + 1))
+    echo "failed ${ct} times"
+    if [ $ct -ge $MAX_RETRY ]; then
+        break
+    fi
+done
