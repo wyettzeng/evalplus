99ab8cf66140c77975094b38bb6f8b821bd1d902
wyett <wyettzeng@gmail.com>
2024-12-28 18:16:44 -0500

    updates

--- myEvaluate/evaluate.py
+++ myEvaluate/evaluate.py
@@ -9,7 +9,8 @@ MODELS = {
     "llama3_instruct_8b": "meta-llama/Meta-Llama-3-8B-Instruct",
     "nxcode_cq_orpo_7b": "NTQAI/Nxcode-CQ-7B-orpo",
     "code_qwen_v1.5_7b": "Qwen/CodeQwen1.5-7B-Chat",
-    "qwen_coder_2.5_7b": "Qwen/Qwen2.5-Coder-7B-Instruct"
+    "qwen_coder_2.5_7b": "Qwen/Qwen2.5-Coder-7B-Instruct",
+    "llama3.1_instruct_8b": "meta-llama/Llama-3.1-8B-Instruct",
 }
 
 
@@ -27,7 +28,7 @@ def evaluate(model_name: str, n_samples:int = 1):
                 dataset=dataset,
                 backend="vllm",
             )
-            sanitize(samples=f"inferenced_output/{dataset}/{model_path_2}_vllm_temp_0.0.jsonl")
+            sanitize(samples=f"inferenced_output/{dataset}/{n_samples}/{model_path_2}_vllm_temp_0.0.jsonl")
         else:
             create_inference(
                 model=model_path,
@@ -39,15 +40,15 @@ def evaluate(model_name: str, n_samples:int = 1):
                 n_samples=n_samples,
                 temperature=1.0,
             )
-            sanitize(samples=f"inferenced_output/{dataset}/{model_path_2}_vllm_temp_1.0.jsonl")
+            sanitize(samples=f"inferenced_output/{dataset}/{n_samples}/{model_path_2}_vllm_temp_1.0.jsonl")
         
     for dataset in ["humaneval", "mbpp"]:
         print(f"-----------{dataset}-{model_name}-------------")
         ram = model_path.replace("/", "--")
         if n_samples == 1:
-            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{ram}_vllm_temp_0.0-sanitized.jsonl")
+            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{n_samples}/{ram}_vllm_temp_0.0-sanitized.jsonl")
         else:
-            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{ram}_vllm_temp_1.0-sanitized.jsonl")
+            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{n_samples}/{ram}_vllm_temp_1.0-sanitized.jsonl")
 
         
 if __name__ == "__main__":
--- myEvaluate/evaluate.sh
+++ myEvaluate/evaluate.sh
@@ -9,6 +9,12 @@ models=(
     "nxcode_cq_orpo_7b"
     "code_qwen_v1.5_7b"
     # "deepseek_coder_v2_16b"
+    "llama3.1_instruct_8b"
+)
+n_lst=(
+    16
+    32
+    64
 )
 model_name=${models[$1]}
 
@@ -22,12 +28,7 @@ do
     fi
 done
 
-ct=0
-until python myEvaluate/evaluate.py --model_name=$model_name --n_samples=16
+for n in ${n_lst[@]}
 do
-    ct=$((ct + 1))
-    echo "failed ${ct} times"
-    if [ $ct -ge $MAX_RETRY ]; then
-        break
-    fi
+    python myEvaluate/evaluate.py --model_name=$model_name --n_samples=${n}
 done
