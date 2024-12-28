202281172c2fe1c5a46566df2cf52e651667ee9b
wyett <wyettzeng@gmail.com>
2024-12-28 18:42:56 -0500

    updates

--- myEvaluate/myEvaluate.py
+++ myEvaluate/myEvaluate.py
@@ -2,6 +2,7 @@ from evalplus.codegen import run_codegen as create_inference
 from evalplus.sanitize import script as sanitize
 from evalplus.evaluate import evaluate as evaluate_model
 from fire import Fire
+
 # tuple in the form of (original model path, original model name, custom model path, custom model name)
 MODELS = {
     "codellama_instruct_7b": "codellama/CodeLlama-7b-Instruct-hf",
@@ -13,8 +14,8 @@ MODELS = {
     "llama3.1_instruct_8b": "meta-llama/Llama-3.1-8B-Instruct",
 }
 
-
 def evaluate(model_name: str, n_samples:int = 1):
+    print(f"Starting inference for {model_name}, n_samples: {n_samples}")
     model_path = MODELS[model_name]
     model_path_2 = model_path.replace("/", "--")
     
@@ -23,32 +24,32 @@ def evaluate(model_name: str, n_samples:int = 1):
             create_inference(
                 model=model_path,
                 greedy=True,
-                root=f"inferenced_output",
+                root=f"inferenced_output/{n_samples}",
                 jsonl_fmt=True,
                 dataset=dataset,
                 backend="vllm",
             )
-            sanitize(samples=f"inferenced_output/{dataset}/{n_samples}/{model_path_2}_vllm_temp_0.0.jsonl")
+            sanitize(samples=f"inferenced_output/{n_samples}/{dataset}/{model_path_2}_vllm_temp_0.0.jsonl")
         else:
             create_inference(
                 model=model_path,
                 greedy=False,
-                root=f"inferenced_output",
+                root=f"inferenced_output/{n_samples}",
                 jsonl_fmt=True,
                 dataset=dataset,
                 backend="vllm",
                 n_samples=n_samples,
                 temperature=1.0,
             )
-            sanitize(samples=f"inferenced_output/{dataset}/{n_samples}/{model_path_2}_vllm_temp_1.0.jsonl")
+            sanitize(samples=f"inferenced_output/{n_samples}/{dataset}/{model_path_2}_vllm_temp_1.0.jsonl")
         
     for dataset in ["humaneval", "mbpp"]:
         print(f"-----------{dataset}-{model_name}-------------")
         ram = model_path.replace("/", "--")
         if n_samples == 1:
-            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{n_samples}/{ram}_vllm_temp_0.0-sanitized.jsonl")
+            evaluate_model(dataset=dataset, samples=f"inferenced_output/{n_samples}/{dataset}/{ram}_vllm_temp_0.0-sanitized.jsonl")
         else:
-            evaluate_model(dataset=dataset, samples=f"inferenced_output/{dataset}/{n_samples}/{ram}_vllm_temp_1.0-sanitized.jsonl")
+            evaluate_model(dataset=dataset, samples=f"inferenced_output/{n_samples}/{dataset}/{ram}_vllm_temp_1.0-sanitized.jsonl")
 
         
 if __name__ == "__main__":
---
+++ myEvaluate/myEvaluate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+MAX_RETRY=3
+models=(
+    "codellama_instruct_7b"
+    "mistral_instruct_v3_7b"
+    "llama3_instruct_8b"
+    "qwen_coder_2.5_7b"
+    "nxcode_cq_orpo_7b"
+    "code_qwen_v1.5_7b"
+    # "deepseek_coder_v2_16b"
+    "llama3.1_instruct_8b"
+)
+n_lst=(
+    # 16
+    32
+    64
+)
+
+# ct=0
+# until python myEvaluate/evaluate.py --model_name=$model_name --n_samples=1
+# do
+#     ct=$((ct + 1))
+#     echo "failed ${ct} times"
+#     if [ $ct -ge $MAX_RETRY ]; then
+#         break
+#     fi
+# done
+
+for model in ${models[@]}
+do
+    for n in ${n_lst[@]}
+    do
+        python myEvaluate/myEvaluate.py --model_name=${model} --n_samples=${n}
+    done
+done
